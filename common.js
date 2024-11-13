185659fd68a4569d3a655dc73b586f4fd309182a
isabele picksius <106542913+barbiebutterfly@users.noreply.github.com>
2024-11-13 19:24:22 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,6 +1,5 @@
 const getCSS = (variavel) => {
-    const valor = getComputedStyle(document.body).getPropertyValue(variavel);
-    return valor || '#222831'; // Caso a variável não exista, retornamos um valor padrão
+    return getComputedStyle(document.body).getPropertyValue(variavel)
 }
 
 const tickConfig = {
@@ -10,31 +9,22 @@ const tickConfig = {
 }
 
 function criarGrafico(data, layout) {
-    const grafico = document.createElement('div');
-    grafico.className = 'grafico';
-    const container = document.getElementById('graficos-container');
-    if (!container) {
-        console.error("Elemento #graficos-container não encontrado!");
-        return;
-    }
-    container.appendChild(grafico);
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
     const config = {
         responsive: true,
         displayModeBar: false
-    };
-    Plotly.newPlot(grafico, data, layout, config);
+    }
+    Plotly.newPlot(grafico, data, layout, config)
 }
 
 function incluirTexto(texto) {
-    const container = document.getElementById('graficos-container');
-    if (!container) {
-        console.error("Elemento #graficos-container não encontrado!");
-        return;
-    }
-    const paragrafo = document.createElement('p');
-    paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = texto;
-    container.appendChild(paragrafo);
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
 }
 
-export { getCSS, tickConfig, criarGrafico, incluirTexto };
+export { getCSS, tickConfig, criarGrafico, incluirTexto }
